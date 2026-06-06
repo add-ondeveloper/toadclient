9a35359f06d83b05ffa68d5ed6af41143d7d05ee
add-ondeveloper <148556014+add-ondeveloper@users.noreply.github.com>
2026-06-06 00:58:30 +0000

    another tiny bugfix

--- Loader/src/Application/ui.cpp
+++ Loader/src/Application/ui.cpp
@@ -114,6 +114,7 @@ namespace toad::ui
                                         } else {
                                             std::cout << "Injection succesful" << std::endl;
                                         }
+                                    }
 
 									if (!failed_shared_mem && !failed_inject)
 										g_is_verified = true;
