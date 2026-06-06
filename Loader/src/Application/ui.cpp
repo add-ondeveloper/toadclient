4974ed7a5aa4ea6a338991b15c92d9db57914843
add-ondeveloper <148556014+add-ondeveloper@users.noreply.github.com>
2026-06-06 00:19:37 +0000

    another bugfix

--- Loader/src/Application/ui.cpp
+++ Loader/src/Application/ui.cpp
@@ -90,7 +90,7 @@ namespace toad::ui
                             // get client type
                             inject_status = "getting client type";
 
-                            g_curr_client = get_client_type(window.title);
+                            g_curr_client = (ClientType)1;
                             std::cout << "currclient type: " << (int)g_curr_client << std::endl;
 							if (init_thread.joinable())
 								init_thread.join();
